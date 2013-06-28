8f78b6a1bcb1f81d69cd1a868b12b5e3ea8763b0
Russell <russell@russelldunphy.com>
2013-06-28 10:58:03 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fiona::Application.config.secret_key_base = '3da8af6dec4d42586c34770b314b7f177b260ec3ef762cd34eab2cf0c32d21f236fb7520d7f335260fa2bc5aa01257f8b2acab33957d202ce7f03587216ac95b'
